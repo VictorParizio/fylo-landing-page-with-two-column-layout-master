5f1bf0362b3aa57c53cd93a1e32f21fd58d98d02
Victor Parizio <victorparizio@hotmail.com>
2024-06-29 23:15:47 -0300

    feat: atualização da thumbnail

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,27 @@
+// const getStartedBtn = document.getElementById('btn-get-started');
+// const enterEmailInput = document.getElementById('enterEmail');
+// const emailInput = document.getElementById('email');
+// const validation1El = document.querySelector('.records');
+// const validation2El = document.querySelector('.records2');
+// const submitBtn = document.getElementById('btn-for-free');
+
+// function validateEmail(inputEl, validationEl) {
+//   if (inputEl.value === '') {
+//     validationEl.classList.add('invalid');
+//   } else {
+//     validationEl.classList.remove('invalid');
+//   }
+// }
+
+// getStartedBtn.addEventListener('click', () => {
+//   validateEmail(enterEmailInput, validation1El);
+// });
+
+// submitBtn.addEventListener('click', () => {
+//   validateEmail(emailInput, validation2El);
+// });
+
+
 const getStarted = document.getElementById('btn-get-started')
 const enterEmail = document.getElementById('enterEmail')
 const checked = document.querySelector('.records')
@@ -21,3 +45,28 @@ enviar.addEventListener('click', () => {
         validacao.classList.remove('invalid')
     }
 })
+
+
+/*const btnGetStarted = document.getElementById('btn-get-started')
+const btnStartedFree = document.getElementById('btn-started-free')
+const email = document.querySelectorAll('.campo')
+const checked = document.querySelectorAll('.records')
+
+function submit() {
+    email.forEach((item, indice) => {
+        if (item.value === '') {
+            checked[indice].classList.add('invalid')
+        } else if (item.value !== '') {
+            checked[indice].classList.remove('invalid')
+        }
+    })
+}
+
+btnGetStarted.addEventListener('click', () => {
+    submit()
+})
+
+btnStartedFree.addEventListener('click', () => {
+    submit()
+})
+*/
